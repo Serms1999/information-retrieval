d87b13ce189f22fd8292bbbf0e527d0fa9d887ae
Sergio Marin Sanchez <serms1999@gmail.com>
2024-04-08 22:21:22 +0200

    Fix evaltools

--- assignment1/evaltools/evaltools.py
+++ assignment1/evaltools/evaltools.py
@@ -1,208 +1,195 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from pandas import Series
 
 
-def generate_relevance_array(relevance_map: dict[set[str], set[str]], categories: Series, relevant_value: int = 1,
-                             non_relevant_value: int = -1) -> np.ndarray[list[int]]:
-    """
-    Generate an array of length `num_queries` containing a list of `relevant_value` for relevant documents and
-     `non_relevant_value` for non-relevant, for each query
-    :param relevance_map: a dictionary that contains the map between the queries and the categories that are relevant
-        for each query
-    :param categories: a pandas Series containing the actual categories of the documents
-    :return: a numpy array containing the relevances for each query
-    """
+def gen_eval_dataset(nq=0, nd=0):
+    if nq == 0:
+        nq = np.random.randint(2, 4)
+    Q = []
+    R = []
+    if nd == 0:
+        nd = np.random.randint(3, 7)
 
-    num_queries: int = len(relevance_map)
-    num_documents: int = len(categories)
+    def generate(nd):
+        keep = True
+        while keep:
+            Q_ = 1 + np.random.permutation(nd)
+            val = np.random.rand(1) * .2 + .4
+            R_ = np.sign(np.random.rand(nd) - val)
+            keep = np.abs(np.sum(R_)) == nd
+        return Q_, R_
 
-    relevances: list[list[int]] = []
-    for query in relevance_map:
-        query_relevances: list[int] = []
-        for document in categories:
-            query_relevances.append(relevant_value if document in relevance_map[query] else non_relevant_value)
-        relevances.append(query_relevances)
-
-    return np.array(relevances)
+    for q in range(nq):
+        Q_, R_ = generate(nd)
+        R.append(R_)
+        Q.append(Q_)
+    return Q, R
 
 
-def evaluate(method: str, queries: list[np.ndarray], relevances: list[np.ndarray]) -> None:
-    """
-    Evaluate the performance of an IR system
-    :param method: the evaluation method to use
-        - `prec_rec`: draw the Precision-Recall curve for each query
-        - `r-prec`: Determine the R-precision for each query
-        - `map`: Calculate the Mean Average Precision
-        - `roc`: Draw the Receiver-Operating-Characteristic for each query
-        - `auc`: Compute the Area Under the ROC curve
-        - `all`: all of the above
-        - `clear`: cleans the solution space
-    :param queries: list of queries, each query is a list of documents
-    :param relevances: list of relevances, each relevancy is a list of documents, 1 for relevant, -1 for non-relevant
-    """
+def generate_ranking(nq, nd):
+    Q, R = random_evaluations(nq, nd)
+    nq = len(Q)
+    nd = len(Q[0])
+    print('Your IR system was issued %d queries from users' % nq)
+    print('Here are the top %d results your IR system retrieved for each query, ' % nd + \
+          'together with the relevance judgements')
+    header = ['q%d\tRel\t\t' % (q + 1) for q in range(nq)]
+    print('d\t' + ''.join(header))
+    for d in range(nd):
+        row = ['%d\t%d\t\t' % (Q[q][d] + 1, 1 if R[q][d] == 1 else 0) for q in range(nq)]
+        print('#%d\t' % (d + 1) + ''.join(row))
+    return Q, R
 
-    num_queries = len(queries)
-    num_documents = len(queries[0])
 
-    # Convert the relevancies to the interval [0, 1]
-    relevances_norm = (np.array(relevances) + 1) / 2
+def evaluate(ex, Q, R):
+    nq = len(Q)
+    nd = len(Q[0])
+    R_ = np.array(R)
+    R_ = .5 * (R_ + 1)
+    Prec_tot = []
+    Rec_tot = []
 
-    def compute_precision_recall(verbose: bool = True) -> tuple[np.ndarray, np.ndarray]:
-        """
-        Compute the precision and recall for each query
-        :param verbose: print the results to the console (default: True)
-        :return:
-        """
+    def compute_PR(print_screen=True):
+        Prec_tot = []
+        Rec_tot = []
+        if print_screen:
+            print('Precision and Recall at k for k=1,...,%d' % nd)
+        for q in range(nq):
+            q1 = q + 1
+            r = R_[q, :]
+            if print_screen:
+                print('\tQuery %d' % q1)
+            Prec_q = []
+            Rec_q = []
+            for k in range(nd):
+                k1 = k + 1
+                Prec = np.sum(r[:k1]) / k1
+                Rec = np.sum(r[:k1]) / np.sum(r)
+                if print_screen:
+                    print('\t\tP(%d)=%d/%d=%.2f,\tR(%d)=%d/%d=%.2f' \
+                          % (k1, np.sum(r[:k1]), k1, Prec, k1, np.sum(r[:k1]), np.sum(r), Rec))
+                Prec_q.append(Prec)
+                Rec_q.append(Rec)
+            Prec_tot.append(Prec_q)
+            Rec_tot.append(Rec_q)
+        Prec_tot = np.array(Prec_tot)
+        Rec_tot = np.array(Rec_tot)
+        return Prec_tot, Rec_tot
 
-        precision_list = []
-        recall_list = []
+    def compute_TPFP(TP_rate=None):
+        TP_tot = []
+        FP_tot = []
+        print('TP_rate and FP_rate at k for k=1,...,%d' % nd)
+        for q in range(nq):
+            q1 = q + 1
+            r = R_[q, :]
+            nr = 1 - r
+            print('\tQuery %d' % q1)
+            TP_q = []
+            FP_q = []
+            for k in range(nd):
+                k1 = k + 1
+                TP = np.sum(r[:k1]) / np.sum(r)
+                FP = np.sum(nr[:k1]) / np.sum(nr)
 
-        if verbose:
-            print(f'Precision and Recall at k for k=1,...,{num_documents}')
+                print('\t\tTP_rate(%d)=R(%d)=%d/%d=%.2f\t FP_rate(%d)=%d/%d=%.2f\t' \
+                      % (k1, k1, np.sum(r[:k1]), np.sum(r), TP, k1, np.sum(nr[:k1]), np.sum(nr), FP))
+                TP_q.append(TP)
+                FP_q.append(FP)
+            TP_tot.append(TP_q)
+            FP_tot.append(FP_q)
+        TP_tot = np.array(TP_tot)
+        FP_tot = np.array(FP_tot)
+        return TP_tot, FP_tot
 
-        for query_index in range(num_queries):
-            query_relevances = relevances_norm[query_index, :]
-            if verbose:
-                print(f'\tQuery {query_index + 1}')
-
-            query_precision = []
-            query_recall = []
-
-            for index in range(1, num_documents + 1):
-                relevant_documents_so_far = np.sum(query_relevances[:index])
-                total_relevant_documents = np.sum(query_relevances)
-
-                precision = relevant_documents_so_far / index
-                recall = relevant_documents_so_far / total_relevant_documents
-
-                if verbose:
-                    print(f'\t\tP({index})={relevant_documents_so_far}/{index}={precision:.2f},\t'
-                          f'R({index})={relevant_documents_so_far}/{total_relevant_documents}={recall:.2f}')
-
-                query_precision.append(precision)
-                query_recall.append(recall)
-            precision_list.append(query_precision)
-            recall_list.append(query_recall)
-
-        return np.array(precision_list), np.array(recall_list)
-
-    def compute_true_positive_rate_false_positive_rate(verbose: bool = True) -> tuple[np.ndarray, np.ndarray]:
-        """
-        Compute the True Positive Rate and False Positive Rate for each query
-        :param verbose: print the results to the console (default: True)
-        :return:
-        """
-
-        tpr_list = []
-        fpr_list = []
-
-        if verbose:
-            print(f'TP_rate and FP_rate at k for k=1,...,{num_documents}')
-
-        for query_index in range(num_queries):
-            query_relevances = relevances_norm[query_index, :]
-            query_non_relevant_documents = 1 - relevances_norm[query_index, :]
-            if verbose:
-                print(f'\tQuery {query_index + 1}')
-
-            query_tpr = []
-            query_fpr = []
-
-            number_of_relevant_documents = np.sum(query_relevances)
-            number_of_non_relevant_documents = np.sum(query_non_relevant_documents)
-
-            for index in range(1, num_documents + 1):
-                tpr_so_far = np.sum(query_relevances[:index]) / number_of_relevant_documents
-                fpr_so_far = np.sum(query_non_relevant_documents[:index]) / number_of_non_relevant_documents
-
-                if verbose:
-                    print(f'\t\tTP_rate({index})=R({index})={np.sum(query_relevances[:index])}'
-                          f'/{number_of_relevant_documents}={tpr_so_far:.2f}\t'
-                          f'FP_rate({index})=NR({index})={np.sum(query_non_relevant_documents[:index])}'
-                          f'/{number_of_non_relevant_documents}={fpr_so_far:.2f}')
-
-                query_tpr.append(tpr_so_far)
-                query_fpr.append(fpr_so_far)
-            tpr_list.append(query_tpr)
-            fpr_list.append(query_fpr)
-
-        return np.array(tpr_list), np.array(fpr_list)
-
-    if method in ('prec_rec', 'all'):
-        total_precision, total_recall = compute_precision_recall()
+    if ex == 'prec_rec' or ex == 'all':
+        Prec_tot, Rec_tot = compute_PR()
         print('\n Draw the Precision-Recall curve for each query')
-
-        for query_index in range(num_queries):
-            print(f'\tQuery {query_index + 1}')
-
+        for q in range(nq):
+            q1 = q + 1
+            print('\tQuery %d' % q1)
             plt.figure()
-            query_recall = total_recall[query_index, :]
-            query_precision = total_precision[query_index, :]
-
-            plt.scatter(query_recall, query_precision)
-            plt.plot(query_recall, query_precision, label='Precision-Recall curve')
-            plt.xlim([-0.05, 1.05])
+            Rec_q = Rec_tot[q, :]
+            Prec_q = Prec_tot[q, :]
+            plt.scatter(np.array(Rec_q), np.array(Prec_q))
+            plt.plot(np.array(Rec_q), np.array(Prec_q), label='Precision-Recall curve')
+            plt.xlim([-0.05, 1.05]);
             plt.ylim([-0.05, 1.05])
-            plt.xlabel('Recall')
+            plt.xlabel('Recall');
             plt.ylabel('Precision')
-
-            interpolated_recall = np.hstack([0, query_recall, 1])
-            interpolated_precision = np.zeros(query_recall.size)
-
-            for i in range(query_recall.size - 1):
-                recall = interpolated_recall[i]
-                if i != 0 and interpolated_recall[i + 1] == recall:
-                    interpolated_precision[i] = np.max(query_precision[i - 1:])
+            R_int = np.hstack([0, Rec_q, 1])
+            P_int = np.zeros(R_int.size)
+            for i_r in range(R_int.size - 1):
+                r = R_int[i_r]
+                if i_r != 0 and R_int[i_r + 1] == r:
+                    P_int[i_r] = np.max(Prec_q[i_r - 1:])
                 else:
-                    interpolated_precision[i] = np.max(query_precision[i:])
-
-            plt.plot(interpolated_recall, interpolated_precision, color='red', label='Interpolated PR curve')
+                    P_int[i_r] = np.max(Prec_q[i_r:])
+            plt.plot(R_int, P_int, color='r', label='Interpolated PR curve')
             plt.legend(loc='lower left')
             plt.show()
-
-    if method in ('r-prec', 'all'):
-        if total_precision is None:
-            total_precision, total_recall = compute_precision_recall()
+    if ex == 'r-prec' or ex == 'all':
+        if Prec_tot == []:
+            Prec_tot, Rec_tot = compute_PR()
         print('\n Determine R-precision for each query')
-
-        for query_index in range(num_queries):
-            query_recall = total_recall[query_index, :]
-            query_precision = total_precision[query_index, :]
-
-            relevant_documents = int(np.sum(relevances_norm[query_index]))
-            print(f'\tQuery {query_index + 1}')
-            print(f'\t\tNumber of relevant documents: {relevant_documents} --> '
-                  f'P({relevant_documents})={query_precision[relevant_documents - 1]:.2f}')
-
-    if method in ('roc', 'all', 'auc'):
-        TP, FP = compute_true_positive_rate_false_positive_rate()
+        for q in range(nq):
+            Rec_q = Rec_tot[q, :]
+            Prec_q = Prec_tot[q, :]
+            r = int(np.sum(R_[q]))
+            q1 = q + 1
+            print('\tQuery %d' % q1)
+            print('\t\tNumber of relevant documents: %d --> P(%d)=%.2f' % (r, r, Prec_q[r - 1]))
+    if ex == 'map' or ex == 'all':
+        if Prec_tot == []:
+            Prec_tot, Rec_tot = compute_PR()
+        print('\n Calculate the Mean Average Precision')
+        APs = []
+        for q in range(nq):
+            Prec_q = Prec_tot[q, :]
+            r = int(np.sum(R_[q]))
+            q1 = q + 1
+            print('\tQuery %d' % q1)
+            str_formula = '1/%d ' % r
+            rs = np.where(R_[q] == 1)[0] + 1
+            str_formula += '{' + ' + '.join(['P(%d)' % rs_ for rs_ in rs]) + '}'
+            AP = np.mean(Prec_q[np.where(R_[q] == 1)])
+            print('\t\tAP=%s=%.2f' % (str_formula, AP))
+            APs.append(AP)
+        APstring = '1/%d {' % nq
+        APstring += ' + '.join(['AP_%d' % (q + 1) for q in range(nq)])
+        APstring += '}=1/%d {' % nq
+        APstring += ' + '.join(['%.2f' % (AP) for AP in APs])
+        APstring += '}'
+        print('\tMAP=%s=%.2f' % (APstring, np.mean(np.array(APs))))
+    if ex == 'roc' or ex == 'all' or ex == 'auc':
+        TP_tot, FP_tot = compute_TPFP()
         print('\n Draw the ROC curve for each query')
-
-        for query_index in range(num_queries):
-            print(f'\tQuery {query_index + 1}')
-
+        for q in range(nq):
+            q1 = q + 1
+            print('\tQuery %d' % q1)
             plt.figure()
-            query_tpr = TP[query_index, :]
-            query_fpr = FP[query_index, :]
-
-            plt.scatter(query_fpr, query_tpr)
-            plt.plot(query_fpr, query_tpr, label='ROC curve')
-            plt.xlim([-0.05, 1.05])
+            TP_q = TP_tot[q, :]
+            FP_q = FP_tot[q, :]
+            plt.scatter(np.array(FP_q), np.array(TP_q))
+            TP_q_ = np.hstack([0, TP_q, 1])
+            FP_q_ = np.hstack([0, FP_q, 1])
+            plt.plot(np.array(FP_q_), np.array(TP_q_), label='ROC curve')
+            plt.xlim([-0.05, 1.05]);
             plt.ylim([-0.05, 1.05])
-            plt.xlabel('FP rate')
+            plt.xlabel('FP rate');
             plt.ylabel('TP rate')
             plt.show()
-
-            if method == 'auc' or method == 'all':
+            if ex == 'auc' or ex == 'all':
                 AUC = []
-                for i in range(query_tpr.size - 1):
-                    delta_x = query_fpr[i + 1] - query_fpr[i]
-                    base = query_tpr[i + 1] + query_tpr[i]
+                for i_x in range(TP_q_.size - 1):
+                    delta_x = FP_q_[i_x + 1] - FP_q_[i_x]
+                    base = TP_q_[i_x + 1] + TP_q_[i_x]
                     AUC.append(base * delta_x / 2)
                 AUC = np.array(AUC)
                 AUC = AUC[AUC > 0]
-                string_AUC = ' + '.join([f'{auc:.2f}' for auc in AUC])
+                string_AUC = ' + '.join(['%.2f' % auc for auc in AUC])
                 if string_AUC != '':
                     string_AUC += ' = '
-                print(f'\tAUC = {string_AUC} {np.sum(AUC):.2f}\n\n')
+                print('\tAUC = %s %.2f\n\n' % (string_AUC, np.sum(AUC)))
+    if ex == 'clear':
+        return
